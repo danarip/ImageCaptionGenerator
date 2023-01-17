4f72bf4586e76c942ab99475035e45a7021303f4
danarip <danarip@gmail.com>
2023-01-17 20:31:21 +0200

    tidy the link2 code

--- link2/KaggleImageCaptioningWithAttention.py
+++ link2/KaggleImageCaptioningWithAttention.py
@@ -2,179 +2,44 @@
 Taken from: https://www.kaggle.com/code/mdteach/torch-data-loader-flicker-8k/notebook
 and from: https://www.kaggle.com/code/mdteach/image-captioning-with-attention-pytorch
 '''
-
-import os
 from datetime import datetime
-import pandas as pd
-from collections import Counter
-from PIL import Image
+import time
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-import torchvision.models as models
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 import torchvision.transforms as T
-from torch.nn.utils.rnn import pad_sequence
 from torch.utils.tensorboard import SummaryWriter
 
-import spacy
-spacy_eng = spacy.load("en_core_web_sm")
-
 from root import cwd
+from link2.data_preprocessing import FlickrDataset, CapsCollate
+from link2.utils import show_image, plot_attention
+from link2.networks import EncoderDecoder
 
 # location of the training data
 data_location = f"{cwd}/data/flickr8k"
-tb = SummaryWriter(log_dir=cwd + "/tensorboard/link2/images_" + datetime.now().strftime("%Y%m%d_%H%M%S"))
+id_run = datetime.now().strftime("%Y%m%d_%H%M%S")
+tb = SummaryWriter(log_dir=cwd + "/tensorboard/link2/images_" + id_run)
 
 
-class Vocabulary:
-    def __init__(self, freq_threshold):
-        # setting the pre-reserved tokens int to string tokens
-        self.itos = {0: "<PAD>", 1: "<SOS>", 2: "<EOS>", 3: "<UNK>"}
-
-        # string to int tokens
-        # its reverse dict self.itos
-        self.stoi = {v: k for k, v in self.itos.items()}
-
-        self.freq_threshold = freq_threshold
-
-    def __len__(self):
-        return len(self.itos)
-
-    @staticmethod
-    def tokenize(text):
-        return [token.text.lower() for token in spacy_eng.tokenizer(text)]
-
-    def build_vocab(self, sentence_list):
-        frequencies = Counter()
-        idx = 4
-
-        for sentence in sentence_list:
-            for word in self.tokenize(sentence):
-                frequencies[word] += 1
-
-                # add the word to the vocab if it reaches minum frequecy threshold
-                if frequencies[word] == self.freq_threshold:
-                    self.stoi[word] = idx
-                    self.itos[idx] = word
-                    idx += 1
-
-    def numericalize(self, text):
-        """ For each word in the text corresponding index token for that word form the vocab built as list """
-        tokenized_text = self.tokenize(text)
-        return [self.stoi[token] if token in self.stoi else self.stoi["<UNK>"] for token in tokenized_text]
-
-
-class FlickrDataset(Dataset):
-    """
-    FlickrDataset
-    """
-
-    def __init__(self, root_dir, captions_file, transform=None, freq_threshold=5):
-        self.root_dir = root_dir
-        self.df = pd.read_csv(captions_file)
-        self.transform = transform
-
-        # Get image and caption colum from the dataframe
-        self.imgs = self.df["image"]
-        self.captions = self.df["caption"]
-
-        # Initialize vocabulary and build vocab
-        self.vocab = Vocabulary(freq_threshold)
-        self.vocab.build_vocab(self.captions.tolist())
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, idx):
-        caption = self.captions[idx]
-        img_name = self.imgs[idx]
-        img_location = os.path.join(self.root_dir, img_name)
-        img = Image.open(img_location).convert("RGB")
-
-        # apply the transfromation to the image
-        if self.transform is not None:
-            img = self.transform(img)
-
-        # numericalize the caption text
-        caption_vec = []
-        caption_vec += [self.vocab.stoi["<SOS>"]]
-        caption_vec += self.vocab.numericalize(caption)
-        caption_vec += [self.vocab.stoi["<EOS>"]]
-
-        return img, torch.tensor(caption_vec)
-
-
-class CapsCollate:
-    """
-    Collate to apply the padding to the captions with dataloader
-    """
-
-    def __init__(self, pad_idx, batch_first=False):
-        self.pad_idx = pad_idx
-        self.batch_first = batch_first
-
-    def __call__(self, batch):
-        imgs = [item[0].unsqueeze(0) for item in batch]
-        imgs = torch.cat(imgs, dim=0)
-
-        targets = [item[1] for item in batch]
-        targets = pad_sequence(targets, batch_first=self.batch_first, padding_value=self.pad_idx)
-        return imgs, targets
-
-
-# defing the transform to be applied
-transforms = T.Compose([
-    T.Resize((224, 224)),
-    T.ToTensor()
-])
-
-dataset = FlickrDataset(
-    root_dir=data_location + "/Images/",
-    captions_file=data_location + "/captions.txt",
-    transform=transforms
-)
-
-# ### 2) **<b>Implementing the Helper function to plot the Tensor image**
-
-# In[15]:
-
-
-# show the tensor image
-import matplotlib.pyplot as plt
-
-
-def show_image(img, title=None, tb=None):
-    """Imshow for Tensor."""
-
-    # unnormalize
-    img[0] = img[0] * 0.229
-    img[1] = img[1] * 0.224
-    img[2] = img[2] * 0.225
-    img[0] += 0.485
-    img[1] += 0.456
-    img[2] += 0.406
-
-    if tb is not None:
-        tb.add_image(title, img)
-    else:
-        img = img.numpy().transpose((1, 2, 0))
-        plt.imshow(img)
-        if title is not None:
-            plt.title(title)
-        plt.pause(0.001)  # pause a bit so that plots are updated
-
-
-# In[34]:
+# helper function to save the model
+def save_model(model2save, num_epochs, id_for_save):
+    model_state = {
+        'num_epochs': num_epochs,
+        'embed_size': embed_size,
+        'vocab_size': len(dataset.vocab),
+        'attention_dim': attention_dim,
+        'encoder_dim': encoder_dim,
+        'decoder_dim': decoder_dim,
+        'state_dict': model2save.state_dict()
+    }
+    torch.save(model_state, f"{cwd}/models/attention_model_state_{id_for_save}.pth")
 
 
 # Initiate the Dataset and Dataloader
-
 # setting the constants
 BATCH_SIZE = 1024
-# BATCH_SIZE = 6
 NUM_WORKER = 4
 
 # defining the transform to be applied
@@ -186,216 +51,27 @@ transforms = T.Compose([
 ])
 
 # testing the dataset class
-dataset = FlickrDataset(
-    root_dir=data_location + "/Images/",
-    captions_file=data_location + "/captions.txt",
-    transform=transforms
-)
+dataset = FlickrDataset(root_dir=data_location + "/Images/", captions_file=data_location + "/captions.txt",
+                        transform=transforms)
 
 # writing the dataloader
-"""
-data_loader = get_data_loader(
-    dataset=dataset,
-    batch_size=BATCH_SIZE,
-    num_workers=NUM_WORKER,
-    shuffle=True,
-    # batch_first=False
-)
-"""
 pad_idx = dataset.vocab.stoi["<PAD>"]
-data_loader = DataLoader(
-    dataset=dataset,
-    batch_size=BATCH_SIZE,
-    num_workers=NUM_WORKER,
-    shuffle=True,
-    collate_fn=CapsCollate(pad_idx=pad_idx, batch_first=True)
-)
-
-# vocab_size
-vocab_size = len(dataset.vocab)
+data_loader = DataLoader(dataset=dataset,
+                         batch_size=BATCH_SIZE,
+                         num_workers=NUM_WORKER,
+                         shuffle=True,
+                         collate_fn=CapsCollate(pad_idx=pad_idx, batch_first=True))
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
-# ### 3) Defining the Model Architecture
-# 
-# Model is seq2seq model. In the **encoder** pretrained ResNet model is used to extract the features. Decoder, is the implementation of the Bahdanau Attention Decoder. In the decoder model **LSTM cell**.
-
-
-class EncoderCNN(nn.Module):
-    def __init__(self):
-        super(EncoderCNN, self).__init__()
-        resnet = models.resnet50(pretrained=True)
-        for param in resnet.parameters():
-            param.requires_grad_(False)
-
-        modules = list(resnet.children())[:-2]
-        self.resnet = nn.Sequential(*modules)
-
-    def forward(self, images):
-        features = self.resnet(images)  # (batch_size,2048,7,7)
-        features = features.permute(0, 2, 3, 1)  # (batch_size,7,7,2048)
-        features = features.view(features.size(0), -1, features.size(-1))  # (batch_size,49,2048)
-        return features
-
-
-# In[26]:
-
-
-# Bahdanau Attention
-class Attention(nn.Module):
-    def __init__(self, encoder_dim, decoder_dim, attention_dim):
-        super(Attention, self).__init__()
-
-        self.attention_dim = attention_dim
-
-        self.W = nn.Linear(decoder_dim, attention_dim)
-        self.U = nn.Linear(encoder_dim, attention_dim)
-
-        self.A = nn.Linear(attention_dim, 1)
-
-    def forward(self, features, hidden_state):
-        u_hs = self.U(features)  # (batch_size,num_layers,attention_dim)
-        w_ah = self.W(hidden_state)  # (batch_size,attention_dim)
-
-        combined_states = torch.tanh(u_hs + w_ah.unsqueeze(1))  # (batch_size,num_layers,attemtion_dim)
-
-        attention_scores = self.A(combined_states)  # (batch_size,num_layers,1)
-        attention_scores = attention_scores.squeeze(2)  # (batch_size,num_layers)
-
-        alpha = F.softmax(attention_scores, dim=1)  # (batch_size,num_layers)
-
-        attention_weights = features * alpha.unsqueeze(2)  # (batch_size,num_layers,features_dim)
-        attention_weights = attention_weights.sum(dim=1)  # (batch_size,num_layers)
-
-        return alpha, attention_weights
-
-
-# In[27]:
-
-
-# Attention Decoder
-class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, vocab_size, attention_dim, encoder_dim, decoder_dim, drop_prob=0.3):
-        super().__init__()
-
-        # save the model param
-        self.vocab_size = vocab_size
-        self.attention_dim = attention_dim
-        self.decoder_dim = decoder_dim
-
-        self.embedding = nn.Embedding(vocab_size, embed_size)
-        self.attention = Attention(encoder_dim, decoder_dim, attention_dim)
-
-        self.init_h = nn.Linear(encoder_dim, decoder_dim)
-        self.init_c = nn.Linear(encoder_dim, decoder_dim)
-        self.lstm_cell = nn.LSTMCell(embed_size + encoder_dim, decoder_dim, bias=True)
-        self.f_beta = nn.Linear(decoder_dim, encoder_dim)
-
-        self.fcn = nn.Linear(decoder_dim, vocab_size)
-        self.drop = nn.Dropout(drop_prob)
-
-    def forward(self, features, captions):
-
-        # vectorize the caption
-        embeds = self.embedding(captions)
-
-        # Initialize LSTM state
-        h, c = self.init_hidden_state(features)  # (batch_size, decoder_dim)
-
-        # get the seq length to iterate
-        seq_length = len(captions[0]) - 1  # Exclude the last one
-        batch_size = captions.size(0)
-        num_features = features.size(1)
-
-        preds = torch.zeros(batch_size, seq_length, self.vocab_size).to(device)
-        alphas = torch.zeros(batch_size, seq_length, num_features).to(device)
-
-        for s in range(seq_length):
-            alpha, context = self.attention(features, h)
-            lstm_input = torch.cat((embeds[:, s], context), dim=1)
-            h, c = self.lstm_cell(lstm_input, (h, c))
-
-            output = self.fcn(self.drop(h))
-
-            preds[:, s] = output
-            alphas[:, s] = alpha
-
-        return preds, alphas
-
-    def generate_caption(self, features, max_len=20, vocab=None):
-        # Inference part
-        # Given the image features generate the captions
-
-        batch_size = features.size(0)
-        h, c = self.init_hidden_state(features)  # (batch_size, decoder_dim)
-
-        alphas = []
-
-        # starting input
-        word = torch.tensor(vocab.stoi['<SOS>']).view(1, -1).to(device)
-        embeds = self.embedding(word)
-
-        captions = []
-
-        for i in range(max_len):
-            alpha, context = self.attention(features, h)
-
-            # store the apla score
-            alphas.append(alpha.cpu().detach().numpy())
-
-            lstm_input = torch.cat((embeds[:, 0], context), dim=1)
-            h, c = self.lstm_cell(lstm_input, (h, c))
-            output = self.fcn(self.drop(h))
-            output = output.view(batch_size, -1)
-
-            # select the word with most val
-            predicted_word_idx = output.argmax(dim=1)
-
-            # save the generated word
-            captions.append(predicted_word_idx.item())
-
-            # end if <EOS detected>
-            if vocab.itos[predicted_word_idx.item()] == "<EOS>":
-                break
-
-            # send generated word as the next caption
-            embeds = self.embedding(predicted_word_idx.unsqueeze(0))
-
-        # covert the vocab idx to words and return sentence
-        return [vocab.itos[idx] for idx in captions], alphas
-
-    def init_hidden_state(self, encoder_out):
-        mean_encoder_out = encoder_out.mean(dim=1)
-        h = self.init_h(mean_encoder_out)  # (batch_size, decoder_dim)
-        c = self.init_c(mean_encoder_out)
-        return h, c
-
-
-# In[28]:
-
-
-class EncoderDecoder(nn.Module):
-    def __init__(self, embed_size, vocab_size, attention_dim, encoder_dim, decoder_dim, drop_prob=0.3):
-        super().__init__()
-        self.encoder = EncoderCNN()
-        self.decoder = DecoderRNN(
-            embed_size=embed_size,
-            vocab_size=len(dataset.vocab),
-            attention_dim=attention_dim,
-            encoder_dim=encoder_dim,
-            decoder_dim=decoder_dim
-        )
-
-    def forward(self, images, captions):
-        features = self.encoder(images)
-        outputs = self.decoder(features, captions)
-        return outputs
-
-
-# ### 4) Setting Hypperparameter and Init the model
-
-# In[29]:
+# 3) Defining the Model Architecture
+"""
+Model is seq2seq model. 
+In the **encoder** pretrained ResNet model is used to extract the features. 
+Decoder, is the implementation of the Bahdanau Attention Decoder. 
+In the decoder model **LSTM cell**.
+"""
 
 
 # Hyperparams
@@ -415,42 +91,25 @@ model = EncoderDecoder(
     vocab_size=len(dataset.vocab),
     attention_dim=256,
     encoder_dim=2048,
-    decoder_dim=512
-).to(device)
+    decoder_dim=512,
+    device=device
+)
+
+model = nn.DataParallel(model, device_ids=[0, 1]).to(device)
 
 criterion = nn.CrossEntropyLoss(ignore_index=dataset.vocab.stoi["<PAD>"])
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 
-# In[31]:
-
-
-# helper function to save the model
-def save_model(model, num_epochs):
-    model_state = {
-        'num_epochs': num_epochs,
-        'embed_size': embed_size,
-        'vocab_size': len(dataset.vocab),
-        'attention_dim': attention_dim,
-        'encoder_dim': encoder_dim,
-        'decoder_dim': decoder_dim,
-        'state_dict': model.state_dict()
-    }
-
-    torch.save(model_state, f"{cwd}/models/attention_model_state.pth")
-
-
-# ## 5) Training Job from above configs
-
-# In[35]:
-
-
-num_epochs = 25
+# 5) Training Job from above configs
+num_epochs = 100
 print_every = 5
 num_batches = len(data_loader)
 num_of_pics_to_show = 3
 
+time_start_training = time.time()
 for epoch in range(num_epochs):
+    time_start_epoch = time.time()
     for idx, (image, captions) in enumerate(data_loader):
         image, captions = image.to(device), captions.to(device)
 
@@ -466,12 +125,13 @@ for epoch in range(num_epochs):
 
         # Backward pass.
         loss.backward()
+        tb.add_scalar("loss", loss.item(), epoch+idx/num_batches)
 
         # Update the parameters in the optimizer.
         optimizer.step()
 
         if idx % print_every == 0:
-            s = f"E{epoch:3}/{num_epochs:3} i{idx:3}/{num_batches:3} loss{loss.item():.5f}"
+            s = f"E{epoch:03d}/{num_epochs:03d} i{idx:03d}/{num_batches:03d} loss{loss.item():.5f}"
             print(s)
 
             # generate the caption
@@ -480,24 +140,24 @@ for epoch in range(num_epochs):
                 for _ in range(num_of_pics_to_show):
                     dataiter = iter(data_loader)
                     img, _ = next(dataiter)
-                    features = model.encoder(img[0:1].to(device))
-                    caps, alphas = model.decoder.generate_caption(features, vocab=dataset.vocab)
+                    features = model.module.encoder(img[0:1].to(device))
+                    caps, alphas = model.module.decoder.generate_caption(features, vocab=dataset.vocab)
                     caption = ' '.join(caps)
-                    show_image(img[0], title=s+ ":" + caption, tb=tb)
+                    show_image(img[0], title=s + ":" + caption, tb=tb)
 
             model.train()
+    time_from_training_start = (time.time() - time_start_training)
+    expected_time = num_epochs / (epoch + 1) * time_from_training_start
+    print(f"epoch time {time_from_training_start / (epoch + 1):.2f} [sec], elapsed_time {time_from_training_start}")
+    print(f"expected total time {expected_time:.2f}")
 
     # save the latest model
-    save_model(model, epoch)
+    save_model(model, epoch, f"{id_run}_{epoch:03d}")
 
 
 # ## 6 Visualizing the attentions
 # Defining helper functions
-# <li>Given the image generate captions and attention scores</li>
-# <li>Plot the attention scores in the image</li>
-
-# In[ ]:
-
+# Given the image generate captions and attention scores. Plot the attention scores in the image
 
 # generate caption
 def get_caps_from(features_tensors):
@@ -513,34 +173,6 @@ def get_caps_from(features_tensors):
 
 
 # Show attention
-def plot_attention(img, result, attention_plot):
-    # untransform
-    img[0] = img[0] * 0.229
-    img[1] = img[1] * 0.224
-    img[2] = img[2] * 0.225
-    img[0] += 0.485
-    img[1] += 0.456
-    img[2] += 0.406
-
-    img = img.numpy().transpose((1, 2, 0))
-    temp_image = img
-
-    fig = plt.figure(figsize=(15, 15))
-
-    len_result = len(result)
-    for l in range(len_result):
-        temp_att = attention_plot[l].reshape(7, 7)
-
-        ax = fig.add_subplot(len_result // 2, len_result // 2, l + 1)
-        ax.set_title(result[l])
-        img = ax.imshow(temp_image)
-        ax.imshow(temp_att, cmap='gray', alpha=0.7, extent=img.get_extent())
-
-    plt.tight_layout()
-    plt.show()
-
-
-# In[ ]:
 
 
 # show any 1
---
+++ link2/utils.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+
+
+def show_image(img, title=None, tb=None):
+    """Imshow for Tensor."""
+
+    # unnormalize
+    img[0] = img[0] * 0.229
+    img[1] = img[1] * 0.224
+    img[2] = img[2] * 0.225
+    img[0] += 0.485
+    img[1] += 0.456
+    img[2] += 0.406
+
+    if tb is not None:
+        tb.add_image(title, img)
+    else:
+        img = img.numpy().transpose((1, 2, 0))
+        plt.imshow(img)
+        if title is not None:
+            plt.title(title)
+        plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+def plot_attention(img, result, attention_plot):
+    # untransform
+    img[0] = img[0] * 0.229
+    img[1] = img[1] * 0.224
+    img[2] = img[2] * 0.225
+    img[0] += 0.485
+    img[1] += 0.456
+    img[2] += 0.406
+
+    img = img.numpy().transpose((1, 2, 0))
+    temp_image = img
+
+    fig = plt.figure(figsize=(15, 15))
+
+    len_result = len(result)
+    for l in range(len_result):
+        temp_att = attention_plot[l].reshape(7, 7)
+
+        ax = fig.add_subplot(len_result // 2, len_result // 2, l + 1)
+        ax.set_title(result[l])
+        img = ax.imshow(temp_image)
+        ax.imshow(temp_att, cmap='gray', alpha=0.7, extent=img.get_extent())
+
+    plt.tight_layout()
+    plt.show()
+
+
