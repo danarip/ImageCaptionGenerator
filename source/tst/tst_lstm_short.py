4e1bba303876defb8e515d62b4ec1104ed8d13bf
danarip <danarip@gmail.com>
2023-01-23 19:46:05 +0200

    Added test for LSTM

---
+++ source/tst/tst_lstm_short.py
@@ -0,0 +1,18 @@
+from source.ImageCaptioning import single_run
+
+if __name__ == "__main__":
+    res = single_run(
+        tb_run_name="tst_lstm_short",
+        run_mode="lstm",
+        freq_threshold=5,
+        num_epochs=10,
+        data_limit=None,
+        batch_size=512,  # Suitable for 2 x A5000 GPUs
+        seq_len=30,  # Sequence length (for padding)
+        embed_size=256,  # Embedding of words
+        # LSTM params
+        attention_dim=256,
+        encoder_dim=2048,
+        decoder_dim=256
+    )
+    print(f"result={res}")
